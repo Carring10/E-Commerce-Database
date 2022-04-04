cb5ac9d1c74b7d2fca85f370f8479673c34bfbe3
Carrington <hartis.c99@gmail.com>
2022-04-04 09:35:43 -0400

    fixed bad requests

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,7 +3,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
     try {
       // be sure to include its associated Product data
@@ -16,20 +16,18 @@ router.get('/', (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
     try {
       // be sure to include its associated Product data
-    const tagData = await Tag.findByPk({
-      include: [{ model: Product }],
-    });
+    const tagData = await Tag.findByPk(req.params.id);
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
     try {
     const tagData = await Tag.create(req.body);
@@ -39,10 +37,10 @@ router.post('/', (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
     try {
-    const updatedTag = await Category.update(req.body, {
+    const updatedTag = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       }
@@ -53,10 +51,10 @@ router.put('/:id', (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
      try {
-    const deletedTag = await Category.delete(req.body, {
+    const deletedTag = await Tag.destroy({
       where: {
         id: req.params.id,
       }
